e5d8941ea0c68adf131e763034a899631fc6c963
Tomas Jansson <tomas.jansson@gmail.com>
2014-11-29 18:25:59 +0100

    exception if trying to create account with non-unique id

--- src/TAAS.Domain/Account.fs
+++ src/TAAS.Domain/Account.fs
@@ -7,9 +7,17 @@ open Commands
 open Events
 open State
 
+exception AccountAlreadyExistsException
+exception InvalidStateException
+
 let handleAccount state ac =
+    let accountState = match state with
+                        | Account s -> s
+                        |_ -> raise InvalidStateException
     match ac with
-    | CreateAccount(accountId, accountName) -> [AccountCreated(accountId, accountName)]
+    | CreateAccount(accountId, accountName) -> 
+        if accountState <> initAccount then raise AccountAlreadyExistsException
+        [AccountCreated(accountId, accountName)]
 
 let evolveAccount p event =
     match event with
