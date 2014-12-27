0c3d4d36a7cf54fa50741ad9232b4e14b96c97d8
Tomas Jansson <tomas.jansson@gmail.com>
2014-12-27 23:51:25 +0100

    added environment

---
+++ environment/manifests/nodes.pp
@@ -0,0 +1,17 @@
+node default {
+	notify { 'YOLO': }
+}
+
+node 'taas-es' {
+	include windowsfunctions
+	include nirvanaservice
+	include eventstore
+	include javaserverjre
+#	include elasticsearch
+	include neo4j
+
+	elasticsearch::plugin { 'marvel': }
+	package { 'procexp':
+		ensure => '15.13',
+	}
+}
